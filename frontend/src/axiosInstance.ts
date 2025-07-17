b5f1696d5752dc4d0e43f397506c8042c5e46fa2
zaidachhwa <zaidachhwa8486@gmail.com>
2025-07-18 01:32:42 +0530

    Chnages again

--- frontend/src/axiosInstance.ts
+++ frontend/src/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://taskdoe-production.up.railway.app";
+const BASE_URL = "https://taskdoe-production.up.railway.app";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
